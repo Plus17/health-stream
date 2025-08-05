a40816c614ce4936fd1895fe8a42ea820616e6ae
Sergio <me@sergiovp.dev>
2025-08-05 11:36:53 -0600

    build(conf): use usec timestamps

--- config/config.exs
+++ config/config.exs
@@ -9,7 +9,7 @@ import Config
 
 config :health_stream,
   ecto_repos: [HealthStream.Repo],
-  generators: [timestamp_type: :utc_datetime]
+  generators: [timestamp_type: :utc_datetime_usec]
 
 # Configures the endpoint
 config :health_stream, HealthStreamWeb.Endpoint,
